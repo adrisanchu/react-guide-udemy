197447c6c99eff87876ae144cead1eae9dd853f6
adrisanchu <adrian.sanchez@ree.es>
2021-10-28 18:00:12 +0200

    #components -> render data from JS into HTML component.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+
+  const expenseDate = new Date(2021, 2, 26);  // month number starts at 0!
+  const expenseTitle = 'Macbook Air';
+  const expenseAmount = 1200;
+
   return (
     <div className="expense-item">
-      <div>26 Marzo 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Macbook Air</h2>
-        <div className="expense-item__price">1200€</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount} €</div>
       </div>
     </div>
   );
