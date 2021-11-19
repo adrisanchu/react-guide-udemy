ac868f8c9c53bdc1238049e3539b3030b3636aa7
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-11-19 16:57:28 +0100

    #state #filterByYear - ExpensesFilter component added. State managed at parent level (Expenses).

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
   const { expenses } = props;
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterYearHandler = (year) => {
+    console.log(year);
+    setFilteredYear(year);
+  };
+
   return (
     <Card className="expenses">
-      {Object.keys(expenses).map( key => (
+      <ExpensesFilter selected={filteredYear} onFilter={filterYearHandler} />
+      {Object.keys(expenses).map((key) => (
         <ExpenseItem
-        title={expenses[key].title}
-        amount={expenses[key].amount}
-        date={expenses[key].date}
-        key={expenses[key].id}
-      />
+          title={expenses[key].title}
+          amount={expenses[key].amount}
+          date={expenses[key].date}
+          key={expenses[key].id}
+        />
       ))}
     </Card>
   );
