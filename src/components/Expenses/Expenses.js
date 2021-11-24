866f23ea50960b7cab1bd3e202baf6137d1f724f
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-11-24 20:05:01 +0100

    #lists #refactor - create new component to handle the logic behind the conditional list to render.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
@@ -16,25 +16,10 @@ const Expenses = (props) => {
     (expense) => expense.date.getFullYear() === Number(filteredYear)
   );
 
-  // dynamic content depending on the length of the contents to display...
-  let expensesContent = <p>No expenses found...</p>;
-
-  if (filteredExpenses.length > 0) {
-    // re-calculate the content to render
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        key={expense.id}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onFilter={filterYearHandler} />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
